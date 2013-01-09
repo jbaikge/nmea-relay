e8b69b6a6411c944beae6c9f03147b6276a68651
jake <bigjake2s@gmail.com>
2013-01-09 09:54:53 -0500

    Increased math when decoding lat/lon stanzas
    Adds better support for devices which provide different lat/lon accuracy

--- convert/NMEALatLon.go
+++ convert/NMEALatLon.go
@@ -1,11 +1,15 @@
 package convert
 
+import (
+	"math"
+)
+
 // Expects []byte("4051.614,N")
 func NMEALat(n []byte) (lat float64) {
-	if len(n) != 10 {
+	if len(n) < 8 {
 		return
 	}
-	lat = toFloat(n[:2]) + toFloat(n[2:4])/60 + toFloat(n[5:8])/3600
+	lat = toFloat(n[:2]) + toFloat(n[2:4])/60 + toFloat(n[5:len(n)-2])/(36*math.Pow10(len(n)-8))
 	if n[len(n)-1] == 'S' {
 		lat *= -1
 	}
@@ -14,10 +18,10 @@ func NMEALat(n []byte) (lat float64) {
 
 // Expects []byte("11944.397,E")
 func NMEALon(n []byte) (lon float64) {
-	if len(n) != 11 {
+	if len(n) < 9 {
 		return
 	}
-	lon = toFloat(n[:3]) + toFloat(n[3:5])/60 + toFloat(n[6:9])/3600
+	lon = toFloat(n[:3]) + toFloat(n[3:5])/60 + toFloat(n[6:len(n)-2])/(36*math.Pow10(len(n)-9))
 	if n[len(n)-1] == 'W' {
 		lon *= -1
 	}
